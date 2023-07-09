fcafd096a431cfc5210d7836233cb99b1c99e8b9
Antonio Fernando <70606080+tony5225-hub@users.noreply.github.com>
2023-07-09 00:25:13 -0300

    atualizado
    atual

--- exercicios/X-MEN/src/js/index.js
+++ exercicios/X-MEN/src/js/index.js
@@ -15,30 +15,46 @@ const personagens = document.querySelectorAll('.personagem');
 personagens.forEach ((personagem) =>{
     personagem.addEventListener('mouseenter',() =>{
 
+        if(window.innerWidth < 450) {
+            window.scrollTo({top: 0, behavior:'smooth'});
+        }
+
         //passo 3 - verificar se já exista um personagem selecionado, se sim, devemos remover a seleção dele 
-        const personagemSelecionado = document.querySelector('.selecionado');
-        personagemSelecionado.classList.remove('selecionado')
+        removerSelecaoDoPersonagem(); // aqui foi criado função se clicar f12 ele vai mostrar emabixo essa função, que é feito selecionando os dizeres com direito mouse/refatorar / dar a função global.
         
         personagem.classList.add('selecionado');
 
         //  OBJETIVO 2 - quando passar o mouse em cima do personagem na listagem, trocar a imagem, o nome e a descrição do personagem grande
         
         //passo 1 - pegar o elemento do personagem grande pra adicionar as informações nele
-
-        const imagemPersonagemGrande = document.querySelector('.personagem-grande');
-
-        // passo 2 - alterar a imagem do personagem grande
-        const idPersonagem = personagem.attributes.id.value;
-         
-        imagemPersonagemGrande.src= `./src/imagens/card-${idPersonagem}.png`;
+        alterarImagemPersonagemSelecionado(personagem); // aqui foi criado função se clicar f12 ele vai mostrar emabixo essa função, que é feito selecionando os dizeres com direito mouse/refatorar / dar a função global.
 
         // passo 3 - alterar o nome do personagem grande
-
-        const nomePersonagem = document.getElementById('nome-personagem');
-        nomePersonagem.innerText = personagem.getAttribute('data-name');
+        alterarNomePersonagemSelecionado(personagem); // aqui foi criado função se clicar f12 ele vai mostrar emabixo essa função, que é feito selecionando os dizeres com direito mouse/refatorar / dar a função global.
 
         // passo 4 - alterar a descrição do personagem grande
-
-        const descricaoPersonagem = document.getElementById('descricao-personagem');
+        newFunction(personagem);
     })
 })
+
+function newFunction(personagem) {
+    const descricaoPersonagem = document.getElementById('descricao-personagem');
+    descricaoPersonagem.innerText = personagem.getAttribute('data-descripton');
+}
+
+function alterarNomePersonagemSelecionado(personagem) {
+    const nomePersonagem = document.getElementById('nome-personagem');
+    nomePersonagem.innerText = personagem.getAttribute('data-name');
+}
+
+function alterarImagemPersonagemSelecionado(personagem) {
+    const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+    // passo 2 - alterar a imagem do personagem grande
+    const idPersonagem = personagem.attributes.id.value;
+    imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
+}
+
+function removerSelecaoDoPersonagem() {
+    const personagemSelecionado = document.querySelector('.selecionado');
+    personagemSelecionado.classList.remove('selecionado');
+}
